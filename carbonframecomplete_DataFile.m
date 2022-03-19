d63724832c96bed08937ba7a2a4be7812fd32f52
guru246810 <90744565+guru246810@users.noreply.github.com>
2022-03-19 09:16:54 +0530

    Add files via upload

---
+++ carbonframecomplete_DataFile.m
@@ -0,0 +1,172 @@
+% Simscape(TM) Multibody(TM) version: 7.1
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(13).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(13).angle = 0.0;
+smiData.RigidTransform(13).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(13).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0.19158204472754731 0.31941493146123617 0.098383489948631053];  % m
+smiData.RigidTransform(1).angle = 0;  % rad
+smiData.RigidTransform(1).axis = [0 0 0];
+smiData.RigidTransform(1).ID = 'B[carbonframepart-1:-:10inchprop-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-1.5902522575952686e-17 1.5620183438064333e-16 0.015000000000000013];  % m
+smiData.RigidTransform(2).angle = 0;  % rad
+smiData.RigidTransform(2).axis = [0 0 0];
+smiData.RigidTransform(2).ID = 'F[carbonframepart-1:-:10inchprop-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [0.33285844817170157 0.074717022785363851 0.098383489948631053];  % m
+smiData.RigidTransform(3).angle = 0;  % rad
+smiData.RigidTransform(3).axis = [0 0 0];
+smiData.RigidTransform(3).ID = 'B[carbonframepart-1:-:10inchprop-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [1.8056988805502411e-15 3.9209712851447781e-16 0.015000000000000013];  % m
+smiData.RigidTransform(4).angle = 0;  % rad
+smiData.RigidTransform(4).axis = [0 0 0];
+smiData.RigidTransform(4).ID = 'F[carbonframepart-1:-:10inchprop-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [0.19158204472754797 -0.16998088589050944 0.098383489948630998];  % m
+smiData.RigidTransform(5).angle = 0;  % rad
+smiData.RigidTransform(5).axis = [0 0 0];
+smiData.RigidTransform(5).ID = 'B[carbonframepart-1:-:10inchprop-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [0 0 0.015000000000000013];  % m
+smiData.RigidTransform(6).angle = 0;  % rad
+smiData.RigidTransform(6).axis = [0 0 0];
+smiData.RigidTransform(6).ID = 'F[carbonframepart-1:-:10inchprop-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-0.090970762160756502 -0.16998088589051102 0.098383489948631053];  % m
+smiData.RigidTransform(7).angle = 0;  % rad
+smiData.RigidTransform(7).axis = [0 0 0];
+smiData.RigidTransform(7).ID = 'B[carbonframepart-1:-:10inchprop-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-2.7323645469034017e-17 8.4816640258331446e-18 0.015000000000000013];  % m
+smiData.RigidTransform(8).angle = 0;  % rad
+smiData.RigidTransform(8).axis = [0 0 0];
+smiData.RigidTransform(8).ID = 'F[carbonframepart-1:-:10inchprop-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [-0.23224716560491271 0.074717022785360632 0.098383489948630998];  % m
+smiData.RigidTransform(9).angle = 1.3560116450761682e-16;  % rad
+smiData.RigidTransform(9).axis = [-1 0 -0];
+smiData.RigidTransform(9).ID = 'B[carbonframepart-1:-:10inchprop-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [6.8240044191813154e-17 1.257462642258125e-17 0.015000000000000013];  % m
+smiData.RigidTransform(10).angle = 1.3560116450761682e-16;  % rad
+smiData.RigidTransform(10).axis = [-0.44509003603055847 -0.8954858233530647 2.7023401288395133e-17];
+smiData.RigidTransform(10).ID = 'F[carbonframepart-1:-:10inchprop-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-0.090970762160759056 0.31941493146123595 0.098383489948631053];  % m
+smiData.RigidTransform(11).angle = 0;  % rad
+smiData.RigidTransform(11).axis = [0 0 0];
+smiData.RigidTransform(11).ID = 'B[carbonframepart-1:-:10inchprop-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [0 0 0.015000000000000013];  % m
+smiData.RigidTransform(12).angle = 0;  % rad
+smiData.RigidTransform(12).axis = [0 0 0];
+smiData.RigidTransform(12).ID = 'F[carbonframepart-1:-:10inchprop-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [0.14215410813970503 0.24582647863063217 0.38257415733114225];  % m
+smiData.RigidTransform(13).angle = 0;  % rad
+smiData.RigidTransform(13).axis = [0 0 0];
+smiData.RigidTransform(13).ID = 'RootGround[carbonframepart-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(2).mass = 0.0;
+smiData.Solid(2).CoM = [0.0 0.0 0.0];
+smiData.Solid(2).MoI = [0.0 0.0 0.0];
+smiData.Solid(2).PoI = [0.0 0.0 0.0];
+smiData.Solid(2).color = [0.0 0.0 0.0];
+smiData.Solid(2).opacity = 0.0;
+smiData.Solid(2).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.12317995070724587;  % kg
+smiData.Solid(1).CoM = [0.050316556403357471 0.074698117243016421 0.11244166383078048];  % m
+smiData.Solid(1).MoI = [0.0014814151905198421 0.0014814151488709861 0.0029540718255088292];  % kg*m^2
+smiData.Solid(1).PoI = [2.1933093815035677e-09 -1.2663077618448931e-09 3.6068955608988418e-11];  % kg*m^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'carbonframepart*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.0035881071564639695;  % kg
+smiData.Solid(2).CoM = [0 0 5.6328828115386287];  % mm
+smiData.Solid(2).MoI = [0.051692500984057248 10.779029421151407 10.777458794289013];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0 -0.0047082432336382077];  % kg*mm^2
+smiData.Solid(2).color = [0 1 0];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = '10inchprop*:*Default';
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(6).Rz.Pos = 0.0;
+smiData.RevoluteJoint(6).ID = '';
+
+smiData.RevoluteJoint(1).Rz.Pos = 129.18688699554801;  % deg
+smiData.RevoluteJoint(1).ID = '[carbonframepart-1:-:10inchprop-1]';
+
+smiData.RevoluteJoint(2).Rz.Pos = -69.516030343617714;  % deg
+smiData.RevoluteJoint(2).ID = '[carbonframepart-1:-:10inchprop-2]';
+
+smiData.RevoluteJoint(3).Rz.Pos = 0;  % deg
+smiData.RevoluteJoint(3).ID = '[carbonframepart-1:-:10inchprop-3]';
+
+smiData.RevoluteJoint(4).Rz.Pos = -58.718640135729082;  % deg
+smiData.RevoluteJoint(4).ID = '[carbonframepart-1:-:10inchprop-4]';
+
+smiData.RevoluteJoint(5).Rz.Pos = -63.570900611729478;  % deg
+smiData.RevoluteJoint(5).ID = '[carbonframepart-1:-:10inchprop-5]';
+
+smiData.RevoluteJoint(6).Rz.Pos = 0;  % deg
+smiData.RevoluteJoint(6).ID = '[carbonframepart-1:-:10inchprop-6]';
+
